ef46decd7e49ccfd3ab7578bd8edb1058e31b9cd
Yaniddze <FaceCrack1337@gmail.com>
2020-09-01 14:03:07 +0500

    Add controller and cookie installers

---
+++ IdentityServer/Server/Installers/ControllersInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server.Installers
+{
+    public class ControllersInstaller: IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+        }
+    }
+}
---
+++ IdentityServer/Server/Installers/CookiesInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server.Installers
+{
+    public class CookiesInstaller: IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Moneys.Identity";
+                config.LoginPath = "/Auth/Login";
+            });
+        }
+    }
+}
