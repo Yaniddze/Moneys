a364528f56b27a0eae5a183593f800be28b0cca9
Yaniddze <FaceCrack1337@gmail.com>
2020-09-01 17:48:53 +0500

    Add login view model

--- IdentityServer/Server/web/src/viewModel/useLoginVM.ts
+++ IdentityServer/Server/web/src/viewModel/useLoginVM.ts
@@ -1,12 +1,59 @@
+import { useState } from 'react';
+
+// Types
+import { LoginResponse } from '../model/types/responses';
+import { LoginUnit } from '../model/types/workers';
+
 // Utils
 import { useTerminated } from '../utils/useTerminated';
 
-type ReturnType = {
-
+type PropTypes = {
+  loginUnit: LoginUnit;
 }
 
-export const useLoginVM = (): ReturnType => {
-  const terminated = useTerminated();
+type FetchLoginType = {
+  isFetching: boolean;
+  data: LoginResponse;
+}
 
-  return {};
+type ReturnType = {
+  loginResponse: FetchLoginType;
+  fetchLogin: (userName: string, password: string) => void;
+}
+
+export const useLoginVM = ({ loginUnit }: PropTypes): ReturnType => {
+  const terminated = useTerminated();
+  
+  const [loginResponse, setLoginResponse] = useState<FetchLoginType>({
+    isFetching: false,
+    data: {
+      errors: [],
+      success: false,
+    },
+  });
+
+  const fetchLogin = (userName: string, password: string): void => {
+    setLoginResponse({
+      isFetching: true,
+      data: {
+        success: false,
+        errors: [],
+      },
+    });
+
+    loginUnit.Invoke(userName, password)
+      .then((result) => {
+        if (!terminated) {
+          setLoginResponse({
+            isFetching: false,
+            data: result,
+          });
+        }
+      });
+  };
+
+  return {
+    loginResponse,
+    fetchLogin,
+  };
 };
