1e1865012f692f97c4d268a97fca371cb966fb06
Yaniddze <FaceCrack1337@gmail.com>
2020-09-06 18:41:48 +0500

    Add form hook

--- IdentityServer/Server/web/src/view/components/forms/LoginForm.tsx
+++ IdentityServer/Server/web/src/view/components/forms/LoginForm.tsx
@@ -1,34 +1,67 @@
 // Core
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 // Components
 import { CentredDiv } from './CentredDiv';
 import { LoginInput } from '../inputs';
 import { StyledForm } from './StyledForm';
 
-export const LoginForm: FC = () => (
-  <CentredDiv>
-    <StyledForm>
+type FormTypes = {
+  login: string;
+  password: string;
+}
 
-      <div>
+export const LoginForm: FC = () => {
+  const { register, handleSubmit, errors } = useForm<FormTypes>();
+
+  const onSubmit = (data: FormTypes): void => {
+    console.log(data);
+  };
+
+  return (
+    <CentredDiv>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+
         <div>
-          Login
+          <p>
+            {errors.login && errors.login.message}
+          </p>
+          <div>
+            Login
+          </div>
+          <LoginInput
+            name="login"
+            ref={register({
+              required: true,
+              maxLength: 50,
+            })}
+          />
         </div>
-        <LoginInput />
-      </div>
 
-      <div>
         <div>
-          Password
+          <p>
+            {errors.password && errors.password.message}
+          </p>
+          <div>
+            Password
+          </div>
+          <LoginInput
+            name="password"
+            type="password"
+            ref={register({
+              required: true,
+              maxLength: 50,
+            })}
+          />
         </div>
-        <LoginInput />
-      </div>
 
-      <div>
-        <button type="submit">
-          Submit
-        </button>
-      </div>
-    </StyledForm>
-  </CentredDiv>
-);
+        <div>
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+      </StyledForm>
+    </CentredDiv>
+  );
+};
