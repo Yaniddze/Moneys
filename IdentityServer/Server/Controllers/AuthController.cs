3446d5fd6dd5bb5258d358f5538de6688c43652c
Yaniddze <FaceCrack1337@gmail.com>
2020-09-11 15:13:02 +0500

    Add google login and registration

--- IdentityServer/Server/Controllers/AuthController.cs
+++ IdentityServer/Server/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using Server.UseCases.Login;
@@ -13,10 +15,18 @@ namespace Server.Controllers
     public class AuthController: Controller
     {
         private readonly IMediator _mediator;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
         
-        public AuthController(IMediator mediator)
+        public AuthController(
+            IMediator mediator,
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager
+            )
         {
             _mediator = mediator;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpPost("login")]
@@ -51,6 +61,81 @@ namespace Server.Controllers
             var response = await _mediator.Send(request);
 
             return Ok(response);
+        }
+
+        [HttpPost("external_login")]
+        public async Task<IActionResult> ExternalLoginAsync([FromBody] string returnUrl)
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            if (info is null)
+            {
+                return Ok(new
+                {
+                    Success = false,
+                    Errors = new[] {"Bad redirect"},
+                });
+            }
+
+            var result = await _signInManager
+                .ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, false);
+
+            if (result.Succeeded)
+            {
+                return Ok(new
+                {
+                    Success = true,
+                });
+            }
+
+            var username = info.Principal.FindFirst(ClaimTypes.Name).Value.Replace(" ", "_");
+            return Ok(new
+            {
+                Success = false,
+                NeedToRegister = true,
+                Username = username,
+                ReturnUrl = returnUrl,
+            });
+        }
+
+        [HttpPost("external_register")]
+        public async Task<IActionResult> ExternalRegisterAsync([FromBody] ExternalRegisterVM vm)
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info is null)
+            {
+                return Ok(new
+                {
+                    Success = false,
+                    Errors = new[] {"Bad redirect"},
+                });
+            }
+            
+            var user = new IdentityUser(vm.Username);
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return Ok(new
+                {
+                    Success = false,
+                    Errors = new[] {"Username is already exists"},
+                });
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            
+            return Ok(new
+            {
+                Success = true,
+                Errors = new[] {"Bad redirect"},
+            });
+        }
+
+        public class ExternalRegisterVM
+        {
+            public string ReturnUrl { get; set; }
+            public string Username { get; set; }
         }
 
         private IEnumerable<string> GetModelStateErrors() => ModelState.Values.Select(x =>
---
+++ IdentityServer/Server/Installers/ExternalAuthInstaller.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer4;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server.Installers
+{
+    public class ExternalAuthInstaller: IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                    options.ClientId =
+                        "http://830770546293-pu13vb9rsqgbh1u4oklhg47p3humh3gr.apps.googleusercontent.com/";
+                    options.ClientSecret = "bPQD_mVEnrilse6HYLfDoi9u";
+                });
+        }
+    }
+}
