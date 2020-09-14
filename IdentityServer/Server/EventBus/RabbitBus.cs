3c3d6bcdb4f907aa6f2229cfe1c3376ef77a8bdd
Yaniddze <FaceCrack1337@gmail.com>
2020-09-14 16:34:42 +0500

    :construction: Implement event bus by rabbitMQ

---
+++ IdentityServer/Server/EventBus/RabbitBus.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Server.EventBus.Abstractions;
+
+namespace Server.EventBus
+{
+    public class RabbitBus: IEventBus
+    {
+        private readonly IConnection _connection;
+        private readonly IServiceProvider _serviceProvider;
+        private IModel _channel;
+
+        public IModel Channel => _channel ??= _connection.CreateModel();
+
+        public RabbitBus(IConnection connection, IServiceProvider serviceProvider)
+        {
+            _connection = connection;
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Publish(IIntegrationEvent @event, string exchangeName)
+        {
+            CreateExchangeIfNotExist(exchangeName);
+
+            var json = JsonConvert.SerializeObject(@event);
+            var bytes = Encoding.UTF8.GetBytes(json);
+            
+            Channel.BasicPublish(exchangeName, string.Empty, body: bytes);
+        }
+
+        public void Subscribe<TEvent, THandler>(string exchangeName, string subscriberName) 
+            where TEvent : IIntegrationEvent 
+            where THandler : IIntegrationEventHandler<TEvent>
+        {
+            BindQueue(exchangeName, subscriberName);
+            
+            var consumer = new AsyncEventingBasicConsumer(Channel);
+
+            consumer.Received += async (obj, args) =>
+            {
+                using var scope = _serviceProvider.CreateScope();
+                
+                var handler = scope.ServiceProvider.GetService<THandler>();
+
+                var jsonRequest = Encoding.UTF8.GetString(args.Body.ToArray());
+                var request = JsonConvert.DeserializeObject<TEvent>(jsonRequest);
+
+                await handler.HandleAsync(request);
+
+                Channel.BasicAck(args.DeliveryTag, false);
+            };
+
+            Channel.BasicConsume(subscriberName, false, consumer);
+        }
+
+        private void CreateExchangeIfNotExist(string exchangeName)
+        {
+            Channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, true);
+        }
+
+        private void BindQueue(string exchangeName, string subscriberName)
+        {
+            CreateExchangeIfNotExist(exchangeName);
+
+            Channel.QueueDeclare(subscriberName, true, false, autoDelete: false);
+            Channel.QueueBind(subscriberName, exchangeName, string.Empty);
+        }
+    }
+}
