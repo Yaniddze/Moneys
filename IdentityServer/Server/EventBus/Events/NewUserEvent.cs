b74a6ceebd4f28f5ea93258da78bdaa6ed8f285f
Yaniddze <FaceCrack1337@gmail.com>
2020-09-14 16:57:12 +0500

    :construction: Add new user event

---
+++ IdentityServer/Server/EventBus/Events/NewUserEvent.cs
@@ -0,0 +1,10 @@
+﻿using Server.EventBus.Abstractions;
+
+namespace Server.EventBus.Events
+{
+    public class NewUserEvent: IIntegrationEvent
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+    }
+}
