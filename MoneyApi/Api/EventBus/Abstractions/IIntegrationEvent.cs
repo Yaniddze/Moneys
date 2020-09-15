e1844412421f1a315d2b96f51e2587c56ba18915
Yaniddze <FaceCrack1337@gmail.com>
2020-09-15 13:20:47 +0500

    :pushpin: Copy event bus implementation from identity server

---
+++ MoneyApi/Api/EventBus/Abstractions/IIntegrationEvent.cs
@@ -0,0 +1,7 @@
+﻿﻿namespace Server.EventBus.Abstractions
+{
+    public interface IIntegrationEvent
+    {
+        
+    }
+}
---
+++ MoneyApi/Api/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,9 @@
+﻿﻿using System.Threading.Tasks;
+
+namespace Server.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<TEvent>
+    {
+        Task HandleAsync(TEvent @event);
+    }
+}
