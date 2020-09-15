1088c64895e0bc8d29e03044dc0f7a065d59ade4
Yaniddze <FaceCrack1337@gmail.com>
2020-09-15 13:22:18 +0500

    :pushpin: Copy rabbitmq injection

---
+++ MoneyApi/Api/ServiceInstallers/RabbitMQInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Api.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using Server.EventBus;
+using Server.EventBus.Abstractions;
+
+namespace Api.ServiceInstallers
+{
+    public class RabbitMQInstaller: IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitConfig = new RabbitMQOptions();
+            var optionsFromConfig = configuration.GetSection(nameof(RabbitMQOptions));
+            
+            optionsFromConfig.Bind(rabbitConfig);
+
+            services.AddSingleton<IEventBus>(new RabbitBus(new ConnectionFactory
+            {
+                HostName = rabbitConfig.Host,
+                Port = rabbitConfig.Port,
+                UserName = rabbitConfig.User,
+                Password = rabbitConfig.Password,
+                DispatchConsumersAsync = true,
+            }, services.BuildServiceProvider()));
+        }
+    }
+}
