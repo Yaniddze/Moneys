83501bba525127fa0d183775a3cea98b200a3f99
Yaniddze <FaceCrack1337@gmail.com>
2020-09-15 13:28:57 +0500

    :sparkles: Add new user event handler

---
+++ MoneyApi/Api/UseCases/EventHandlers/NewUserEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Server.EventBus.Abstractions;
+using Server.EventBus.Events;
+
+namespace Api.UseCases.EventHandlers
+{
+    public class NewUserEventHandler: IIntegrationEventHandler<NewUserEvent>
+    {
+        public static int UsersCount = 0;
+        
+        public async Task HandleAsync(NewUserEvent @event)
+        {
+            UsersCount++;
+        }
+    }
+}
