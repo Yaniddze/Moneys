f65e370adbce0dc565b14e9472f997cf0599c1b3
Yaniddze <FaceCrack1337@gmail.com>
2020-09-15 13:29:32 +0500

    :wrench: Inject new user event handler

---
+++ MoneyApi/Api/ServiceInstallers/EventHandlersInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Api.UseCases.EventHandlers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Server.EventBus.Abstractions;
+using Server.EventBus.Events;
+
+namespace Api.ServiceInstallers
+{
+    public class EventHandlersInstaller: IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IIntegrationEventHandler<NewUserEvent>, NewUserEventHandler>();
+        }
+    }
+}
