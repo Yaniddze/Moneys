936629886b0722628090726519bc9356bdad62af
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 21:55:48 +0500

    :construction: Add di for mapper

---
+++ MoneyApi/Api/ServiceInstallers/AutoMapperInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Api.AutoMappers.Extensions;
+using Api.ServiceInstallers.Abstractions;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceInstallers
+{
+    public class AutoMapperInstaller: IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(options => options.InstallFromAssembly());
+        }
+    }
+}
