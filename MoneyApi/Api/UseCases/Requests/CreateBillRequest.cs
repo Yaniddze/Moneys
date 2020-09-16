9bf94576cfa71187aa356eb7613a11879f67b845
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 19:51:36 +0500

    :construction: Add create bill request

---
+++ MoneyApi/Api/UseCases/Requests/CreateBillRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace Api.UseCases.Requests
+{
+    public class CreateBillRequest: IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
