b35986e70f7c6a339c3d35223d32f6a64a207110
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 18:32:48 +0500

    :construction: Add update bill request

---
+++ MoneyApi/Api/UseCases/Requests/UpdateBillTitleRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace Api.UseCases.Requests
+{
+    public class UpdateBillTitleRequest: IRequest<bool>
+    {
+        public Guid UserId { get; set; }
+        public Guid BillId { get; set; }
+    }
+}
