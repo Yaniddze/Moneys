c47e68bd2cd574c2f2e81f78dab6d1d1792d8e98
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 19:56:27 +0500

    :construction: Create abstract answer

---
+++ MoneyApi/Api/UseCases/Requests/Abstractions/AbstractAnswer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Api.UseCases.Requests.Abstractions
+{
+    public class AbstractAnswer
+    {
+        public bool Success { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+
+    public class AbstractAnswer<TData>
+    {
+        public bool Success { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+        public TData Data { get; set; }
+    }
+}
