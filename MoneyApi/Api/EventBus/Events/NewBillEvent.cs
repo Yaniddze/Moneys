584ca78f77db62e49e44ab7a008234de72d11057
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 19:51:21 +0500

    :construction: Add new bill event

---
+++ MoneyApi/Api/EventBus/Events/NewBillEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Api.EventBus.Abstractions;
+
+namespace Api.EventBus.Events
+{
+    public class NewBillEvent: IIntegrationEvent
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
