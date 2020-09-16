20d29ea5d191e47750d38413fa59ecf200c30415
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 18:30:41 +0500

    :construction: Add create/delete bill requests

---
+++ MoneyApi/Api/UseCases/Requests/NewBillRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MediatR;
+
+namespace Api.UseCases.ManualCases
+{
+    public class NewBillRequest: IRequest<bool>
+    {
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
