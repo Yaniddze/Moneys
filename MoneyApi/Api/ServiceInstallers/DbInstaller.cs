e4afdbf3d20f3ca758e80e49d03fabbac086ba9b
Yaniddze <FaceCrack1337@gmail.com>
2020-09-16 21:21:49 +0500

    :construction: Add db di

---
+++ MoneyApi/Api/ServiceInstallers/DbInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Api.DataBase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceInstallers
+{
+    public class DbInstaller: IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            var connString = configuration.GetConnectionString("PostgresConnection");
+
+            services.AddDbContextPool<MoneysContext>(options => options.UseNpgsql(connString));
+        }
+    }
+}
