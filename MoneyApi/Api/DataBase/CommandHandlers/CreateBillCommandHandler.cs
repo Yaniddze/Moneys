891dc5740b01bdf1663ab2ef068061cb04cb6e13
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 15:47:07 +0500

    :construction: Add create bill command handler

---
+++ MoneyApi/Api/DataBase/CommandHandlers/CreateBillCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.DataBase.DbEntities;
+using Api.UseCases.Abstractions;
+using Api.UseCases.Commands;
+using MediatR;
+
+namespace Api.DataBase.CommandHandlers
+{
+    public class CreateBillCommandHandler: IRequestHandler<CreateBillCommand, AbstractAnswer<Guid>>
+    {
+        private readonly MoneysContext _context;
+
+        public CreateBillCommandHandler(MoneysContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AbstractAnswer<Guid>> Handle(CreateBillCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var tempId = Guid.NewGuid();
+
+                _context.Bills.Add(new BillDB
+                {
+                    Id = tempId,
+                    Title = request.Title,
+                    UserId = request.UserId,
+                });
+
+                await _context.SaveChangesAsync(cancellationToken);
+                
+                return new AbstractAnswer<Guid>
+                {
+                    Success = true,
+                    Data = tempId,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AbstractAnswer<Guid>
+                {
+                    Success = false,
+                    Errors = new[] { ex.Message }
+                };
+            }
+        }
+    }
+}
