2660544918c369cfc8ede590aebacb7794ed7c0f
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 16:07:26 +0500

    :construction: Add update bill command handler

---
+++ MoneyApi/Api/DataBase/CommandHandlers/UpdateBillCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.DataBase.DbEntities;
+using Api.UseCases.Abstractions;
+using Api.UseCases.Commands;
+using MediatR;
+using Z.EntityFramework.Plus;
+
+namespace Api.DataBase.CommandHandlers
+{
+    public class UpdateBillCommandHandler: IRequestHandler<UpdateBillCommand, AbstractAnswer>
+    {
+        private readonly MoneysContext _context;
+
+        public UpdateBillCommandHandler(MoneysContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AbstractAnswer> Handle(UpdateBillCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var updatedCount = await _context.Bills
+                    .Where(x => x.Id == request.BillId && x.UserId == request.UserId)
+                    .UpdateAsync(x => new BillDB
+                    {
+                        Title = request.NewTitle,
+                    }, cancellationToken);
+
+                if (updatedCount > 0)
+                {
+                    return new AbstractAnswer
+                    {
+                        Success = true,
+                    };
+                }
+                
+                return new AbstractAnswer
+                {
+                    Success = false,
+                    Errors = new []{ "Bad request params" }
+                };
+            }
+            catch (Exception e)
+            {
+                return new AbstractAnswer
+                {
+                    Success = false,
+                    Errors = new []{ "Database error" },
+                };
+            }
+        }
+    }
+}
