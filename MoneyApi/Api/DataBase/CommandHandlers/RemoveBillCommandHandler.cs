70a3bfed4431f434b6069aae0f81095cd43d3356
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 15:58:29 +0500

    :construction: Add remove bill command handler

---
+++ MoneyApi/Api/DataBase/CommandHandlers/RemoveBillCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.UseCases.Abstractions;
+using Api.UseCases.Commands;
+using MediatR;
+using Z.EntityFramework.Plus;
+
+namespace Api.DataBase.CommandHandlers
+{
+    public class RemoveBillCommandHandler: IRequestHandler<RemoveBillCommand, AbstractAnswer>
+    {
+        private readonly MoneysContext _context;
+
+        public RemoveBillCommandHandler(MoneysContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AbstractAnswer> Handle(RemoveBillCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var deletedCount = await _context.Bills
+                    .Where(x => x.Id == request.BillId && x.UserId == request.UserId)
+                    .DeleteAsync(cancellationToken);
+
+                if (deletedCount > 0)
+                {
+                    return new AbstractAnswer
+                    {
+                        Success = true
+                    };
+                }
+                
+                return new AbstractAnswer
+                {
+                    Success = false,
+                    Errors = new []{ "Bad user_id or bill_id" }
+                };
+            }
+            catch (Exception e)
+            {
+                return new AbstractAnswer
+                {
+                    Success = false,
+                    Errors = new []{ "Database error" }
+                };
+            }
+        }
+    }
+}
