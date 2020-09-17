2d1e27ba6191c3af59f9d503f9843e32c59c6d68
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 19:46:05 +0500

    :construction: Add create transaction command

---
+++ MoneyApi/Api/UseCases/Commands/TransactionCommands/CreateTransactionCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.Commands.TransactionCommands
+{
+    public class CreateTransactionCommand: IRequest<AbstractAnswer<Guid>>
+    {
+        public Guid UserId { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public Guid BillId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
