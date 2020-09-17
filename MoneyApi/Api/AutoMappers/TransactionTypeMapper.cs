b93622c300e9b7cc1aadd687dbf1c0b85388c3df
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 20:09:45 +0500

    :construction: Add Transaction Type Mapper

---
+++ MoneyApi/Api/AutoMappers/TransactionTypeMapper.cs
@@ -0,0 +1,33 @@
+﻿using Api.AutoMappers.Abstractions;
+using Api.DataBase.DbEntities;
+using Api.Domain;
+using AutoMapper;
+
+namespace Api.AutoMappers
+{
+    public class TransactionTypeMapper: IMapperInstaller
+    {
+        public void Install(IMapperConfigurationExpression options)
+        {
+            options.CreateMap<TransactionType, TransactionTypeDB>()
+                .ForMember(x => x.Id,
+                    map => map.MapFrom(
+                        dest => dest.Id
+                    ))
+                .ForMember(x => x.Title,
+                    map => map.MapFrom(
+                        dest => dest.Title
+                    ));
+            
+            options.CreateMap<TransactionTypeDB, TransactionType>()
+                .ForMember(x => x.Id,
+                    map => map.MapFrom(
+                        dest => dest.Id
+                    ))
+                .ForMember(x => x.Title,
+                    map => map.MapFrom(
+                        dest => dest.Title
+                    ));
+        }
+    }
+}
