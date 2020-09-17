49334e6ead2f5aad71413db0b243b0e32be99fe3
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 19:46:17 +0500

    :construction: Add create transaction event

---
+++ MoneyApi/Api/EventBus/Events/TransactionEvents/NewTransactionEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Api.EventBus.Abstractions;
+
+namespace Api.EventBus.Events.TransactionEvents
+{
+    public class NewTransactionEvent: IIntegrationEvent
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public Guid BillId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
