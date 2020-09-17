3d893cecc131e9697ce00c5c02222cde28ca85bf
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 21:18:53 +0500

    :construction: Add get transactions type feature

---
+++ MoneyApi/Api/DataBase/CommandHandlers/GetTransactionTypesCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.DataBase.DbEntities;
+using Api.Domain;
+using Api.UseCases.Abstractions;
+using Api.UseCases.Commands;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Api.UseCases.Abstractions.AbstractAnswer<System.Collections.Generic.IEnumerable<Api.Domain.TransactionType>>;
+
+namespace Api.DataBase.CommandHandlers
+{
+    public class GetTransactionTypesCommandHandler: IRequestHandler<GetTransactionTypesCommand, AbstractAnswer<IEnumerable<TransactionType>>>
+    {
+        private readonly IMapper _mapper;
+        private readonly MoneysContext _context;
+
+        public GetTransactionTypesCommandHandler(MoneysContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<AbstractAnswer<IEnumerable<TransactionType>>> Handle(GetTransactionTypesCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var founded = await _context.TransactionTypes
+                    .Select(x => _mapper.Map<TransactionTypeDB, TransactionType>(x))
+                    .ToListAsync(cancellationToken);
+                
+                return CreateSuccess(founded);
+            }
+            catch (Exception e)
+            {
+                return CreateFailed(new[] {"Database error"});
+            }
+        }
+    }
+}
---
+++ MoneyApi/Api/UseCases/Commands/GetTransactionTypesCommand.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Api.Domain;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.Commands
+{
+    public class GetTransactionTypesCommand: IRequest<AbstractAnswer<IEnumerable<TransactionType>>>
+    { }
+}
