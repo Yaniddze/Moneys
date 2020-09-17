05f8de80abe7f2cfe9d812c29fcc1447f85f0fa1
Yaniddze <FaceCrack1337@gmail.com>
2020-09-17 19:46:31 +0500

    :construction: Add new transaction use case

---
+++ MoneyApi/Api/UseCases/ManualCases/NewTransaction/NewTransactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.ManualCases.NewTransaction
+{
+    public class NewTransactionRequest: IRequest<AbstractAnswer<Guid>>
+    {
+        public Guid UserId { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public Guid BillId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MoneyApi/Api/UseCases/ManualCases/NewTransaction/NewTransactionUseCase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.EventBus.Abstractions;
+using Api.EventBus.Events.TransactionEvents;
+using Api.UseCases.Abstractions;
+using Api.UseCases.Commands.TransactionCommands;
+using MediatR;
+
+namespace Api.UseCases.ManualCases.NewTransaction
+{
+    public class NewTransactionUseCase: IRequestHandler<NewTransactionRequest, AbstractAnswer<Guid>>
+    {
+        private readonly IMediator _mediator;
+        private readonly IEventBus _eventBus;
+        
+        public NewTransactionUseCase(IMediator mediator, IEventBus eventBus)
+        {
+            _mediator = mediator;
+            _eventBus = eventBus;
+        }
+
+        public async Task<AbstractAnswer<Guid>> Handle(NewTransactionRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(new CreateTransactionCommand
+            {
+                BillId = request.BillId,
+                Date = request.Date,
+                Description = request.Description,
+                UserId = request.UserId,
+                Value = request.Value,
+            }, cancellationToken);
+
+            if (response.Success)
+            {
+                _eventBus.Publish(new NewTransactionEvent
+                {
+                    Id = response.Data,
+                    BillId = request.BillId,
+                    Date = request.Date,
+                    Description = request.Description,
+                    UserId = request.UserId,
+                    Value = request.Value,
+                }, nameof(NewTransactionEvent));
+            }
+
+            return response;
+        }
+    }
+}
