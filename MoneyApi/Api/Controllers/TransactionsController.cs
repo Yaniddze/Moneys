e84e2967fb4eeb384f5a40d8bb5d259578f04327
Yaniddze <FaceCrack1337@gmail.com>
2020-09-18 18:17:12 +0500

    :sparkles: Add transactions controller

---
+++ MoneyApi/Api/Controllers/TransactionsController.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Api.UseCases.Commands.TransactionCommands;
+using Api.UseCases.ManualCases.NewTransaction;
+using Api.UseCases.ManualCases.RemoveTransaction;
+using Api.UseCases.ManualCases.UpdateTransaction;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/v1/transactions")]
+    public class TransactionsController: Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TransactionsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetTransactionAsync([FromQuery] GetTransactionsCommand request)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPut("add")]
+        public async Task<IActionResult> AddTransactionAsync([FromBody] NewTransactionRequest request)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPatch("update")]
+        public async Task<IActionResult> UpdateTransactionAsync([FromBody] UpdateTransactionRequest request)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteTransactionAsync([FromQuery] RemoveTransactionRequest request)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
