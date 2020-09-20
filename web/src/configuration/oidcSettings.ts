e20826ce4c99f8a9d13dc5a0794a06211ccb7769
Yaniddze <FaceCrack1337@gmail.com>
2020-09-21 01:08:43 +0500

    :fire: Add userinfo path

--- web/src/configuration/oidcSettings.ts
+++ web/src/configuration/oidcSettings.ts
@@ -3,6 +3,8 @@ import { VuexOidcClientSettings } from 'vuex-oidc';
 export const oidcSettings: VuexOidcClientSettings = {
   authority: 'https://yaniddze.com',
   clientId: 'vue_application',
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  client_secret: 'secret_key',
   redirectUri: 'http://localhost:8080/callback',
   responseType: 'code',
   scope: 'openid profile email offline_access',
