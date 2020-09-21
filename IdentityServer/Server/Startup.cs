96b1ee3ef76541f1a1b456a79cb34fa875308d78
Yaniddze <FaceCrack1337@gmail.com>
2020-09-22 00:29:12 +0500

    :construction: Add forwarded headers

--- IdentityServer/Server/Startup.cs
+++ IdentityServer/Server/Startup.cs
@@ -41,6 +41,14 @@ namespace Server
                 MinimumSameSitePolicy = SameSiteMode.None,
                 Secure = CookieSecurePolicy.Always,
             });
+            
+            var options = new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All,
+            };
+            options.KnownProxies.Add(IPAddress.Any);
+
+            app.UseForwardedHeaders(options);
 
             app.UseAuthentication();
 
