458396ff351f3d8301063efdb471e64398daf299
Yaniddze <FaceCrack1337@gmail.com>
2020-09-21 22:57:42 +0500

    :construction: Add request logging

---
+++ IdentityServer/Server/Installers/ForwardedHeadersInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server.Installers
+{
+    public class ForwardedHeadersInstaller: IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardLimit = 2;
+                options.KnownProxies.Add(IPAddress.Parse("127.0.10.1"));
+                options.ForwardedForHeaderName = "X-Forwarded-For-My-Custom-Header-Name";
+            });
+        }
+    }
+}
--- IdentityServer/Server/Startup.cs
+++ IdentityServer/Server/Startup.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using IdentityServer4.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Server.Installers;
 
 namespace Server
@@ -45,6 +47,32 @@ namespace Server
             app.Use(async (ctx, next) =>
             {
                 ctx.SetIdentityServerOrigin("https://yaniddze.com");
+                await next();
+            });
+
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var logger = scope.ServiceProvider.GetService<ILogger<Startup>>();
+            
+             app.Use(async (context, next) =>
+            {
+                //context.Response.ContentType = "text/plain";
+                
+                // Request method, scheme, and path
+                logger.LogInformation("Request Method: {context.Request.Method}", context.Request.Method);
+                logger.LogInformation("Request Scheme: {context.Request.Scheme}", context.Request.Scheme);
+                logger.LogInformation("Request Path: {context.Request.Path}", context.Request.Path);
+
+                // Headers
+                logger.LogInformation( "Request Headers:");
+
+                foreach (var header in context.Request.Headers)
+                {
+                    logger.LogInformation( "{header.Key}: {header.Value}", header.Key, header.Value);
+                }
+
+                // Connection: RemoteIp
+                logger.LogInformation("Request RemoteIp: {context.Connection.RemoteIpAddress}", context.Connection.RemoteIpAddress);
                 await next();
             });
             
