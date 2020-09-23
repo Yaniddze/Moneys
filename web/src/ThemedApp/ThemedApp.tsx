b2142b5449cc615b049debf76c3adc7241e847f4
Yaniddze <FaceCrack1337@gmail.com>
2020-09-23 21:45:02 +0500

    :art: Add themed app

---
+++ web/src/ThemedApp/ThemedApp.tsx
@@ -0,0 +1,58 @@
+// Core
+import React, {
+  FC,
+  ReactElement,
+  useState,
+} from 'react';
+import Cookies from 'js-cookie';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+
+// Components
+import { ToggleSwitch } from '../views/components/switches';
+
+// Themes
+import { lightTheme } from './themes/lightTheme';
+import { darkTheme } from './themes/darkTheme';
+
+type PropType = {
+  children: ReactElement;
+};
+
+const themeCookieName = 'Moneys.Theme';
+const lightValue = 'light';
+const darkValue = 'dark';
+
+const cookedTheme = Cookies.get(themeCookieName);
+const lightCooked = cookedTheme === lightValue;
+
+export const ThemedApp: FC<PropType> = ({
+  children,
+}: PropType) => {
+  const [currentTheme, setCurrentTheme] = useState<DefaultTheme>(
+    lightCooked ? lightTheme : darkTheme,
+  );
+
+  const handleChange = (value: boolean): void => {
+    if (value) {
+      Cookies.set(themeCookieName, darkValue);
+      setCurrentTheme(darkTheme);
+    } else {
+      Cookies.set(themeCookieName, lightValue);
+      setCurrentTheme(darkTheme);
+    }
+  };
+
+  return (
+    <div>
+      <ThemeProvider theme={currentTheme}>
+        <ToggleSwitch
+          initValue={!lightCooked}
+          handleChange={handleChange}
+        />
+        <div>
+          {children}
+        </div>
+      </ThemeProvider>
+    </div>
+  );
+};
---
+++ web/src/ThemedApp/themes/lightTheme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  colors: {
+    main: 'white',
+    secondary: 'gray',
+    dim: 'gray',
+    dark: 'black',
+    calcDarkInRgba: (opacity): string => `rgba(120,120,120, ${opacity})`,
+    error: 'red',
+    linkColor: 'blue',
+  },
+
+  font: {
+    normalSize: '16px',
+    smallerSize: '13px',
+    btnFontSize: '15px',
+
+    btnFontColor: 'black',
+  },
+};
