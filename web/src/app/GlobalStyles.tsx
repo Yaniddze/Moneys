66b73023b5e225c87a467f7e28396e47024f114c
Yaniddze <FaceCrack1337@gmail.com>
2020-09-24 23:18:17 +0500

    :art: Add normalizing

--- web/src/app/GlobalStyles.tsx
+++ web/src/app/GlobalStyles.tsx
@@ -1,10 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  body {
+  body, html {
+    display: block;
+    position: relative;
     margin: 0;
     padding: 0;
     font-family: 'Roboto', sans-serif;
     background: ${(props): string => (props.theme.colors.dim)};
+    width: 100%;
+    height: 100%;
+  }
+  
+  #root {
+    height: 100%;
   }
 `;
