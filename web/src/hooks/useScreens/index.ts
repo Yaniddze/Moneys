5d16bd79b214b1b6685ba526bec7741f16faf7db
Yaniddze <FaceCrack1337@gmail.com>
2020-09-30 21:17:39 +0500

    :art: Add header box-shadow

---
+++ web/src/hooks/useScreens/index.ts
@@ -0,0 +1,1 @@
+export { useScreens } from './useScreens';
--- web/src/views/components/header/StyledHeader.tsx
+++ web/src/views/components/header/StyledHeader.tsx
@@ -5,6 +5,8 @@ export const StyledHeader = styled.header`
   background: ${(props): string => props.theme.colors.primary.color};
   color: ${(props): string => props.theme.colors.primary.contrast};
   
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  
   display: flex;
   flex-direction: row;
   
