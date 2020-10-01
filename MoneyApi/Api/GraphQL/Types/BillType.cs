46e9ee63da6aaeec2c72a1d55bf1276414591670
Yaniddze <FaceCrack1337@gmail.com>
2020-10-02 00:31:40 +0500

    :wrench: Add bill type

---
+++ MoneyApi/Api/GraphQL/Types/BillType.cs
@@ -0,0 +1,18 @@
+﻿using Api.Domain;
+using HotChocolate.Types;
+
+namespace Api.GraphQL.Types
+{
+    public class BillType: ObjectType<Bill>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Bill> descriptor)
+        {
+            descriptor.Field(x => x.Id)
+                .Type<NonNullType<IdType>>();
+
+            descriptor.Field(x => x.Title)
+                .Type<StringType>()
+                .Name("title");
+        }
+    }
+}
