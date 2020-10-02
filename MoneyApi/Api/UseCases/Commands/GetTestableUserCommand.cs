f10cb042849041505b861a0f5aa573315a22bf73
Yaniddze <FaceCrack1337@gmail.com>
2020-10-03 00:45:37 +0500

    :sparkles: Add query handler

---
+++ MoneyApi/Api/UseCases/Commands/GetTestableUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using Api.Domain;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.Commands
+{
+    public class GetTestableUserCommand: IRequest<AbstractAnswer<User>>
+    {
+        
+    }
+}
