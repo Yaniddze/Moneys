4b5c9a79fd4bbbb0a7bf83e608a84da76b48af1e
Yaniddze <FaceCrack1337@gmail.com>
2020-10-03 00:24:50 +0500

    :art: Add documentation

--- MoneyApi/Api/GraphQL/RootMutations.cs
+++ MoneyApi/Api/GraphQL/RootMutations.cs
@@ -7,6 +7,7 @@ using Api.UseCases.ManualCases.RemoveBill;
 using Api.UseCases.ManualCases.RemoveTransaction;
 using Api.UseCases.ManualCases.UpdateBill;
 using Api.UseCases.ManualCases.UpdateTransaction;
+using HotChocolate;
 using MediatR;
 
 namespace Api.GraphQL
@@ -21,17 +22,19 @@ namespace Api.GraphQL
         }
 
         #region Bill
-
+        [GraphQLDescription("Create new bill for user and return new bill's id")]
         public async Task<AbstractAnswer<Guid>> CreateBillAsync(NewBillRequest request)
         {
             return await mediator.Send(request);
         }
-
-        public async Task<AbstractAnswer> DeleteBillAsync(RemoveBillRequest request)
+        
+        [GraphQLDescription("Delete bill by id and return true on success")]
+        public async Task<AbstractAnswer> RemoveBillAsync(RemoveBillRequest request)
         {
             return await mediator.Send(request);
         }
-
+        
+        [GraphQLDescription("Update bill by id and return true on success")]
         public async Task<AbstractAnswer> UpdateBillAsync(UpdateBillRequest request)
         {
             return await mediator.Send(request);
@@ -40,17 +43,20 @@ namespace Api.GraphQL
         #endregion
 
         #region Transactions
-
+        
+        [GraphQLDescription("Create new transaction for user and return new transaction's id")]
         public async Task<AbstractAnswer<Guid>> AddTransactionAsync(NewTransactionRequest request)
         {
             return await mediator.Send(request);
         }
-
+        
+        [GraphQLDescription("Update transaction by id and return true on success")]
         public async Task<AbstractAnswer> UpdateTransactionAsync(UpdateTransactionRequest request)
         {
             return await mediator.Send(request);
         }
-
+        
+        [GraphQLDescription("Delete transaction by id and return true on success")]
         public async Task<AbstractAnswer> RemoveTransactionAsync(RemoveTransactionRequest request)
         {
             return await mediator.Send(request);
--- MoneyApi/Api/GraphQL/RootQuery.cs
+++ MoneyApi/Api/GraphQL/RootQuery.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Api.Domain;
 using Api.UseCases.Abstractions;
 using Api.UseCases.Commands;
 using Api.UseCases.Commands.BillsCommands;
 using Api.UseCases.Commands.TransactionCommands;
+using HotChocolate;
 using MediatR;
 
 namespace Api.GraphQL
@@ -18,17 +18,19 @@ namespace Api.GraphQL
         {
             this.mediator = mediator;
         }
-        
+        [GraphQLDescription("Returns all user's bills by user id")]
         public async Task<AbstractAnswer<IEnumerable<Bill>>> GetBillsAsync(GetBillsCommand command)
         {
             return await mediator.Send(command);
         }
         
+        [GraphQLDescription("Returns all transaction types")]
         public async Task<AbstractAnswer<IEnumerable<TransactionType>>> GetTransactionTypesAsync()
         {
             return await mediator.Send(new GetTransactionTypesCommand());
         }
 
+        [GraphQLDescription("Returns all user's transactions by user id")]
         public async Task<AbstractAnswer<IEnumerable<Transaction>>> GetTransactionsAsync(GetTransactionsCommand command)
         {
             return await mediator.Send(command);
