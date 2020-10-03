9e522f230bd361f381db9f808b4748b2acd93b75
Yaniddze <FaceCrack1337@gmail.com>
2020-10-04 03:14:33 +0500

    :art: Rename routes

--- web/src/configuration/navbarConfig.tsx
+++ web/src/configuration/navbarConfig.tsx
@@ -2,19 +2,19 @@ import { Nav } from '../views/components/header/navbar/types';
 
 export const navs: Nav[] = [
   {
-    path: '/app',
-    title: '13',
-  },
-  {
     path: '/home',
-    title: '13',
+    title: 'Домой',
   },
   {
-    path: '/ap1p',
-    title: '32',
+    path: '/bills',
+    title: 'Счета',
   },
   {
-    path: '/app2',
-    title: '22',
+    path: '/transactions',
+    title: 'Транзакции',
+  },
+  {
+    path: '/stats',
+    title: 'Статистика',
   },
 ];
