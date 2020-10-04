ea1925d68ff09507dec24b54e54247ac7309cacf
Yaniddze <FaceCrack1337@gmail.com>
2020-10-05 03:07:38 +0500

    :sparkles: Add bills output

--- web/src/views/pages/BillsPage/BillsPage.tsx
+++ web/src/views/pages/BillsPage/BillsPage.tsx
@@ -3,22 +3,40 @@ import React, { FC } from 'react';
 
 // Components
 import { Title } from '../styles';
+import { SimpleLoader } from '../../components/loaders';
 
 // Hooks
 import { usePageWrapper } from '../hooks/usePageWrapper';
+import { useBills } from '../../../hooks/useBills';
 
 type PropTypes = {
   children?: never;
 }
 
 export const BillsPage: FC<PropTypes> = () => {
+  const { state } = useBills();
   const { Wrapper } = usePageWrapper();
+
+  const loader = state.fetching && <SimpleLoader />;
+  const items = !state.fetching
+    && state.data.success
+    && state.data.bills.map((bill) => (
+      <div key={bill.id}>
+        { bill.title }
+      </div>
+    ));
 
   return (
     <Wrapper>
-      <Title>
-        124
-      </Title>
+      <div>
+        { loader }
+        <Title>
+          124
+        </Title>
+        <div>
+          { items }
+        </div>
+      </div>
     </Wrapper>
   );
 };
