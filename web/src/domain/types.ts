4629b3a13078d21e15d07de33b8dd27e47928042
Yaniddze <FaceCrack1337@gmail.com>
2020-10-06 03:27:36 +0500

    :construction: Add domain objects

--- web/src/domain/types.ts
+++ web/src/domain/types.ts
@@ -2,3 +2,21 @@ export type Bill = {
   id: string;
   title: string;
 }
+
+export type Transaction = {
+  bill: Bill;
+  id: string;
+  type: TransactionType;
+  info: TransactionInfo;
+}
+
+export type TransactionType = {
+  id: string;
+  title: string;
+}
+
+export type TransactionInfo = {
+  date: Date;
+  description: string;
+  value: number;
+}
