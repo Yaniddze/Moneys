fad6e268ab42f836dc27b04245654e29e2df7077
Zinevich Yan <facecrack1337@gmail.com>
2020-10-18 21:31:25 +0500

    :wrench: Add apollo error handling

--- front/app/apollo/Apollo.tsx
+++ front/app/apollo/Apollo.tsx
@@ -1,6 +1,10 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+// Hooks
 import { useUserStorage } from '../../hooks/storage/useUserStorage';
+import { useUserManager } from '../../hooks/useUserManager';
 
 import { createClient } from './apolloConfig';
 
@@ -12,7 +16,14 @@ export const Apollo: FC<PropTypes> = ({
   children,
 }: PropTypes) => {
   const { user } = useUserStorage();
+  const { manager } = useUserManager();
   const client = createClient(user?.access_token);
+
+  useEffect(() => {
+    onError(() => {
+      manager.signoutRedirect();
+    });
+  }, [manager]);
 
   return (
     <ApolloProvider client={client}>
